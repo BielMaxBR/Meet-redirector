5f192bb820dfbee389dc6ff735d4861820f4956b
BielMaxBR <gbielmaximo@gmail.com>
2020-05-20 11:14:24 -0300

    removing test comments

--- background.js
+++ background.js
@@ -1,9 +1,9 @@
 var _redirectURL = "";
-// "https://aula-digital.plurall.net/hangouts/*"
+
 chrome.webRequest.onHeadersReceived.addListener(function (details) {
   if (_redirectURL == "") {
     console.log(details)
-      _redirectUrl = details.url.substring(48); //.substring(details.initiator.indexOf("room=h") + 6)
+      _redirectUrl = details.url.substring(48);
 
       return { redirectUrl: _redirectUrl /*Redirection URL*/ };
   }
