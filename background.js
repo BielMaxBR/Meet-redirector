037cbf2fbf92a235bab19d717f922faddb3f3fc0
BielMaxBR <gbielmaximo@gmail.com>
2020-05-21 08:25:26 -0300

    clipboard

--- background.js
+++ background.js
@@ -3,10 +3,26 @@ var _redirectURL = "";
 chrome.webRequest.onHeadersReceived.addListener(function (details) {
   if (_redirectURL == "") {
     console.log(details)
-      _redirectUrl = details.url.substring(48);
-
-      return { redirectUrl: _redirectUrl /*Redirection URL*/ };
+    _redirectUrl = details.url.substring(48);
+    copyTextToClipboard(_redirectUrl)
+    return { redirectUrl: _redirectUrl /*Redirection URL*/ };
   }
 }, {
   urls: ["http://*/*", "https://*/*"]
 }, ["blocking", "responseHeaders"]);
+
+function copyTextToClipboard(text) {
+  var copyFrom = document.createElement("textarea");
+
+  copyFrom.textContent = text;
+
+  document.body.appendChild(copyFrom);
+
+  copyFrom.select();
+
+  document.execCommand('copy');
+
+  copyFrom.blur();
+
+  document.body.removeChild(copyFrom);
+}
